9c8b32c54847de1fc70bdc9b175e5083ccc108a1
Sergey Matevitsky <2828878@gmail.com>
2019-05-21 09:43:09 +0300

    controller package added

---
+++ src/main/java/com/matevitsky/controller/ListServlet.java
@@ -0,0 +1,16 @@
+package com.matevitsky.controller;
+
+/*
+public class ListServlet extends HttpServlet {
+
+    UserService userService = new UserServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("userNames", userService.getAll());
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/list.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+}
+*/
